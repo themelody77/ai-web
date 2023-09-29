324ccca4e63240ed737735b68e3b0d93868dd6d8
SVS Sathvik <svssathvik@SVSs-MacBook-Air.local>
2023-09-29 18:40:00 +0530

    extractive and abstractive working2

--- abstract_summary.py
+++ abstract_summary.py
@@ -2,12 +2,12 @@ import os
 import google.generativeai as palm
 
 # palm.configure(api_key=os.environ.get('API_KEY'))
-palm.configure(api_key="AIzaSyDp5fkGPgw8VeKa5oN_AJqM7MezZOg8VR4")
+palm.configure(api_key=os.environ.get("YOUR_API_KEY"))
 
 models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
 model = models[0].name
 def getAbstractSummary(rawtext,size):
-    rawtext += "\n Summarize the above for a "+int(size)*2+" mark question"
+    rawtext += "\n Summarize the above for a "+str(int(size)*2)+" mark question"
     completion = palm.generate_text(
         model=model,
         prompt=rawtext,
